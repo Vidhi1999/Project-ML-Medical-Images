527bc4bfddbfcf1ca5b321ec916374de7d688514
Vidhi Chhatbar <vidhi.chhatbar@spit.ac.in>
2021-08-08 22:15:59 +0530

    secure project

--- project/project/settings.py
+++ project/project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!4dxv5(%ge+moh9ore7&q!85!531@*%*2w+-xbp95y9ggq-q6$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
